3077bc06bdd91fb5e3354444e8e54111ab3dd3f4
Noah Sugarman <zetalmac@gmail.com>
2013-07-09 01:04:01 -0700

    touch update

--- keybindings.js
+++ keybindings.js
@@ -4,13 +4,18 @@
     configure: function() {
       atom.input.bind(atom.key.LEFT_ARROW, 'left');
       atom.input.bind(atom.key.A, 'left');
+      atom.input.bind(atom.touch.SWIPE_LEFT, 'left');
       atom.input.bind(atom.key.RIGHT_ARROW, 'right');
       atom.input.bind(atom.key.D, 'right');
+      atom.input.bind(atom.touch.SWIPE_RIGHT, 'right');
       atom.input.bind(atom.key.UP_ARROW, 'up');
       atom.input.bind(atom.key.W, 'up');
+      atom.input.bind(atom.touch.SWIPE_UP, 'up');
       atom.input.bind(atom.key.DOWN_ARROW, 'down');
       atom.input.bind(atom.key.S, 'down');
+      atom.input.bind(atom.touch.SWIPE_DOWN, 'down');
       atom.input.bind(atom.key.SPACE, 'swap');
+      atom.input.bind(atom.touch.TAP, 'swap');
       return atom.input.bind(atom.key.P, 'pause');
     }
   };
